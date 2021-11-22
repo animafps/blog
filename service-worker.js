ef2e74dd2f8320a8c50f562c2c962eadcf591662
Anima <18208134+animafps@users.noreply.github.com>
2021-11-22 20:14:44 +1300

    improved service worker to fully precache

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+// let Service Worker take control of pages ASAP
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+// let Workbox handle our precache list
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
