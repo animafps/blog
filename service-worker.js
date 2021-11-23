141481a535a469afeb9bc3c3e28f36f06c2c4d6b
Anima <18208134+animafps@users.noreply.github.com>
2021-11-23 13:01:24 +1300

    Minified the service worker script

--- service-worker.js
+++ service-worker.js
@@ -5,6 +5,11 @@ workbox.core.clientsClaim();
 // let Workbox handle our precache list
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
+workbox.routing.registerRoute(
+  /service-register.js/,
+  new workbox.strategies.NetworkOnly()
+);
+
 workbox.recipes.pageCache();
 workbox.recipes.staticResourceCache();
 workbox.recipes.imageCache();
