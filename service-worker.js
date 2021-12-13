3d07a1d123429f20beb2a96b235c1c23a2da8974
Anima <18208134+animafps@users.noreply.github.com>
2021-12-13 16:55:23 +1300

    removed precache service worker

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,7 @@
-// let Service Worker take control of pages ASAP
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
-// let Workbox handle our precache list
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
+workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
 
 workbox.routing.registerRoute(
   /service-register.js/,
