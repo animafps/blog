e3fc4aeb64ae642d0ebbc11964579f93a72ff8e3
Anima <18208134+animafps@users.noreply.github.com>
2021-12-20 20:03:53 +1300

    added home page and reworked dark mode toggle

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const socials: { name: string; href: string; rel?: string }[] = [
+  { name: "twitter", href: "https://twitter.com/animafps" },
+  { name: "youtube", href: "https://youtube.com/c/animafps" },
+  { name: "source", href: "https://github.com/animafps/blog" },
+  { name: "rss", href: "/rss.xml" },
+];
