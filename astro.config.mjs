517dd920600598b72cef06b7f40793f89e53acac
Anima <18208134+animafps@users.noreply.github.com>
2022-03-28 11:17:04 +1300

    First Blood

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,45 @@
-// Full Astro Configuration API Documentation:
+import svelte from "@astrojs/svelte"; // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
-
 // @ts-check
-export default /** @type {import('Astro').AstroUserConfig} */ {
+
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+
+import turbolinks from "@astrojs/turbolinks";
+
+// https://astro.build/config
+export default defineConfig(
+/** @type {import('Astro').AstroUserConfig} */
+{
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: "https://animafps.xyz", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-    drafts: false,
+    site: "https://animafps.xyz",
+    // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true,
+    // Generate sitemap (set to "false" to disable)
+    drafts: false
   },
-  renderers: ["@astrojs/renderer-svelte"],
+  integrations: [svelte(), sitemap(), partytown(), turbolinks()],
   vite: {
     optimizeDeps: {
-      allowNodeBuiltins: true,
-    },
+      allowNodeBuiltins: true
+    }
   },
   markdownOptions: {
-    render: [
-      "@astrojs/markdown-remark",
-      {
-        remarkPlugins: ["remark-smartypants", "remark-gfm"],
-        rehypePlugins: [
-          "rehype-slug",
-          [
-            "rehype-autolink-headings",
-            {
-              behavior: "prepend",
-            },
-          ],
-          "rehype-external-links",
-        ],
-      },
-    ],
-  },
-};
+    render: ["@astrojs/markdown-remark", {
+      remarkPlugins: ["remark-smartypants", "remark-gfm"],
+      rehypePlugins: ["rehype-slug", ["rehype-autolink-headings", {
+        behavior: "prepend"
+      }], "rehype-external-links"]
+    }]
+  }
+});
--- public/functions/script.js.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(request) {
-  var url = new URL(request.url);
-  url.hostname = "static.cloudflareinsights.com";
-  url.pathname = "/beacon.min.js";
-  let response = await fetch(url, request);
-  const newResponse = new Response(response.body, response);
-  return newResponse;
-}
